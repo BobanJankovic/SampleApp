6b582cbd74471c8667cb7495e812914109e5f093
bobi <boban0092@gmail.com>
2018-06-12 20:37:54 +0200

    safasfasf

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "drinks#index"
   resources :drinks
+  delete '/drinks/:id' => 'drinks#destroy', as: :delete_drink 
 end
